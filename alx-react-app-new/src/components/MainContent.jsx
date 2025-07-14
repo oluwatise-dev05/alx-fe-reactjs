153730f9511008a1936a2892c13da6225e8c8c71
Oluwatise-cloud <tisebae1@gmail.com>
2025-07-14 22:07:43 +0100

    Apply inline styling to all components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', fontSize: '18px', textAlign: 'center', backgroundColor: '#eef' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
+
+
